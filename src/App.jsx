3a4c5575b6c06d51b2badb0684cd61bb30fa7fcc
Valentina <valentona1996@gmail.com>
2025-07-18 12:58:26 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hola desde React</h1>
+      <p>¡Mi primera página web con React!</p>
+    </div>
+  );
+}
+
+export default App;
